8bdcea2511039f609b00806625fd54a1be47ec6e
José ismael correia <62517288+Ismael1995Developer@users.noreply.github.com>
2025-09-23 10:56:00 -0300

    fix: falha no next.js e vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  images: {
+    domains: ["ismael-correia.dev"],
+    formats: ["image/webp", "image/avif"],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
